1738a911e80c420555a134005b030db31e9c4c31
NotAkshat <23bit005@ietdavv.edu.in>
2025-09-16 17:19:41 +0530

    implement registration

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
+
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
