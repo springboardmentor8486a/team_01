155c0dbc2e755425aa47f879efe4e3e14ad40fa2
Niroshini842 <nirokumarano15@gmail.com>
2025-09-19 19:44:55 +0530

    Fix: Correct package.json scripts and dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+    server: {
+    port: 3000 // or another port of your choice
+  }
+});
