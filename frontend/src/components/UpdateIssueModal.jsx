0179f08de5e1f0c077b882dccf8093d17c1456a7
Bharath444444 <bharathkumargottapu444@gmail.com>
2025-10-01 15:01:11 +0530

    issue changes

--- frontend/src/components/UpdateIssueModal.jsx
+++ frontend/src/components/UpdateIssueModal.jsx
@@ -44,7 +44,7 @@ const UpdateIssueModal = ({ issue, onClose }) => {
                 <option value="in progress">In Progress</option>
                 <option value="resolved">Resolved</option>
                 <option value="pending">Pending</option>
-                <option value="new">New</option>
+      
               </select>
             </div>
 
